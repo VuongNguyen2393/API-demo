d4ae61e02cc36b91c917076e9499ce2b0b489179
VuongNguyen2393 <nguyendinhvuong.act@gmail.com>
2021-11-12 06:51:09 +0700

    update

--- src/controllers/controller_product.ts
+++ src/controllers/controller_product.ts
@@ -2,5 +2,6 @@ const Product = require('../models/products')
 
 exports.post_product = function(req:any,res:any){
     const post = new Product(req.body)
-    post.save()
+    post.save().then(() => console.log('data updated'))
 }
+
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,6 @@ const router = express_1.Router()
 const {post_product} = require('../controllers/controller_product')
 
 
-
+router.post('/product',post_product)
 
 module.exports = router;
