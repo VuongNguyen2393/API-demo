afe74ae6a2b3b0414acd60f506af07c28f74e6aa
VuongNguyen2393 <nguyendinhvuong.act@gmail.com>
2021-11-12 00:57:58 +0700

    update

--- index.ts
+++ index.ts
@@ -1,19 +1,33 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const routes = require('./src/routes/routes')
+
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+const db = mongoose.connection
 
-app.get('/', (req:any, res:any) => {
-    res.send({'vuong':'get something - VuongND9'})
+dotenv.config()
+
+
+// connect DB
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true})
+
+app.use(bodyParser.json())
+
+app.use('/',routes)
+//=============Chưa test được DB nên test tạm cái này====================
+app.get('/product', (req:any, res:any) => {
+    res.json({  Product_name:'quạt',
+                Categofy:'gia dụng',
+                Price: '120.000'})
   })
-
-app.post('/product',(req:any,res:any) => {
-    res.send('product list - VuongND9')
-})
-
-app.get('/product',(req:any,res:any) => {
-    res.send('product list get - VuongND9')
-})
+//=============================================================================
 
 app.listen(port, () => {
-    console.log(`Sever VuongND9 is listening on port ${port}`)
+    console.log(`Sever API - VuongND9 is listening on port ${port}`)
 })
+
+module.exports = app
