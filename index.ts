5a7012307c0cded1fb2c938451b51116f3e78df2
VuongNguyen2393 <nguyendinhvuong.act@gmail.com>
2021-11-11 08:53:57 +0700

    huhu

---
+++ index.ts
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req:any, res:any) => {
+    res.send('get something - VuongND9')
+  })
+
+app.post('/product',(req:any,res:any) => {
+    res.send('product list - VuongND9')
+})
+
+app.listen(port, () => {
+    console.log(`Sever VuongND9 is listening on port ${port}`)
+})
